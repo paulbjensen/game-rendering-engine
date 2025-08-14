81d0f12a3ae4fe27a8cf3cdeb8125adc46f1c5d2
Paul Jensen <paulbjensen@gmail.com>
2025-08-14 13:44:05 +0100

    Changed paths to get the app to run nicely

--- src/mapAndTiles.ts
+++ src/mapAndTiles.ts
@@ -5,20 +5,20 @@ import { loadImage, loadJSON } from './utils';
     the code to reference the correct image, as well as lookup the tile size.
 */
 const tilesLibrary = [
-    { code: 0, name: 'grass', image: loadImage('img/grass.png'), width: 64, height: 32 },
-    { code: 1, name: 'road', image: loadImage('img/road.png'), width: 64, height: 32 },
-    { code: 2, name: 'water', image: loadImage('img/water.png'), width: 64, height: 32 },
-    { code: 3, name: 'dot', image: loadImage('img/dot.png'), width: 64, height: 64 },
-    { code: 4, name: 'dot-only', image: loadImage('img/dot-only.png'), width: 64, height: 64 },
-    { code: 5, name: 'water-round-top', image: loadImage('img/water-round-top.png'), width: 64, height: 32 },
-    { code: 6, name: 'water-round-left', image: loadImage('img/water-round-left.png'), width: 64, height: 32 },
-    { code: 7, name: 'water-round-bottom', image: loadImage('img/water-round-bottom.png'), width: 64, height: 32 },
-    { code: 8, name: 'water-round-right', image: loadImage('img/water-round-right.png'), width: 64, height: 32 },
-    { code: 9, name: 'road-with-markings-tl-br', image: loadImage('img/road-with-markings-tl-br.png'), width: 64, height: 32 },
-    { code: 10, name: 'road-with-markings-bl-tr', image: loadImage('img/road-with-markings-bl-tr.png'), width: 64, height: 32 },
-    { code: 11, name: 'sand', image: loadImage('img/sand.png'), width: 64, height: 32 },
-    { code: 12, name: 'english-townhouse', image: loadImage('img/english-townhouse.png'), width: 64, height: 64 },
-    { code: 13, name: 'cornershop', image: loadImage('img/cornershop.png'), width: 64, height: 64 }
+    { code: 0, name: 'grass', image: loadImage('/img/grass.png'), width: 64, height: 32 },
+    { code: 1, name: 'road', image: loadImage('/img/road.png'), width: 64, height: 32 },
+    { code: 2, name: 'water', image: loadImage('/img/water.png'), width: 64, height: 32 },
+    { code: 3, name: 'dot', image: loadImage('/img/dot.png'), width: 64, height: 64 },
+    { code: 4, name: 'dot-only', image: loadImage('/img/dot-only.png'), width: 64, height: 64 },
+    { code: 5, name: 'water-round-top', image: loadImage('/img/water-round-top.png'), width: 64, height: 32 },
+    { code: 6, name: 'water-round-left', image: loadImage('/img/water-round-left.png'), width: 64, height: 32 },
+    { code: 7, name: 'water-round-bottom', image: loadImage('/img/water-round-bottom.png'), width: 64, height: 32 },
+    { code: 8, name: 'water-round-right', image: loadImage('/img/water-round-right.png'), width: 64, height: 32 },
+    { code: 9, name: 'road-with-markings-tl-br', image: loadImage('/img/road-with-markings-tl-br.png'), width: 64, height: 32 },
+    { code: 10, name: 'road-with-markings-bl-tr', image: loadImage('/img/road-with-markings-bl-tr.png'), width: 64, height: 32 },
+    { code: 11, name: 'sand', image: loadImage('/img/sand.png'), width: 64, height: 32 },
+    { code: 12, name: 'english-townhouse', image: loadImage('/img/english-townhouse.png'), width: 64, height: 64 },
+    { code: 13, name: 'cornershop', image: loadImage('/img/cornershop.png'), width: 64, height: 64 }
 ];
 
 // TODO - we could move this into a Map class, which will allow us to load maps with different numbers of rows and columns, base_tile heights/widths (for more detailed maps), and either generate random maps or load from a file
@@ -41,7 +41,7 @@ const generateRandomMap = (numberOfRows:number, numberOfColumns:number) => {
 // const map = generateRandomMap(128,128);
 
 // Load the map data from a JSON file
-const map = await loadJSON('/data/16x16.json');
+const map = await loadJSON('/maps/16x16.json');
 
 /*
     We define the map dimensions here to help with drawing 
