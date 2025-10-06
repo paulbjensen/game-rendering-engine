474e779da4521c28da9ebef7219e44a8052c337a
Paul Jensen <paulbjensen@gmail.com>
2025-10-06 15:26:13 +0100

    Added a missing bit of code from a previous commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
+import * as Sentry from "@sentry/svelte";
 import { mount } from "svelte";
 import App from "./App.svelte";
+
+Sentry.init({
+	dsn: "https://f5c21ee8e3324bec1e7443982acca134@o95413.ingest.us.sentry.io/4510142976688128",
+	// Setting this option to true will send default PII data to Sentry.
+	// For example, automatic IP address collection on events
+	sendDefaultPii: true,
+});
 
 const appElement: HTMLElement | null = document.getElementById("app");
 
