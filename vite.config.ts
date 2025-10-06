0312d697f773e4646a22dcc012da7b1b4d68fdb4
Paul Jensen <paulbjensen@gmail.com>
2025-10-06 15:25:43 +0100

    Added some more Sentry-based configuration for uploading sourcemaps during the build phase

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		svelte(),
-	],
+	plugins: [svelte(), sentryVitePlugin({
+        org: "anephenix",
+        project: "babsland"
+    })],
 
 	build: {
 		sourcemap: true,
